cc02123ebe5e802f6340e31f0c07ef39c2434c33
anas <amellaheanas@gmail.com>
2023-09-19 18:01:05 +0100

    fix2

--- _convert_base.c
+++ _convert_base.c
@@ -4,14 +4,15 @@
 * @nbr: struct
 * @base: forma
 * @count: count
+* @len: len
 * Return: 1
 */
-void _convert_base(long int nbr, char *base, int *count, int len)
+int _convert_base(long int nbr, char *base, int *count, int len)
 {
 	if (nbr >= len)
 	{
 		*count += 1;
-		_convert_base(nbr / len, base, count);
+		_convert_base(nbr / len, base, count, len);
 	}
 	_putchar(base[nbr % len]);
 	return (1);
